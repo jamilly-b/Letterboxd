2b86dfbed10dd70fcc72897be0687b7afe30abdf
jamilly-b <jbm2@discente.ifpe.edu.br>
2024-05-17 17:04:50 -0300

    first commit

---
+++ src/test/java/com/jamilly/movies/MoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jamilly.movies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
